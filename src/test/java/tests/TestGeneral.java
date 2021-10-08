ac5008a5b45307cc7c2025a711591642b4c83c5f
Joseco65 <jacortinaf@gmail.com>
2021-10-07 19:38:56 -0500

    Envio las pruebas unitarias
    Ya incluí el TestGeneral en el paquete de pruebas test, para probar tienen que cambiar el encabezado de @Ignore a @Test

---
+++ src/test/java/tests/TestGeneral.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests;
+
+import logica.Producto;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
+import persistencia.ConexionBD;
+import persistencia.ProductoDAO;
+
+/**
+ *
+ * @author Grupo optimus
+ */
+public class TestGeneral { 
+    @Ignore
+    public void verificarConexion() {
+        ConexionBD con = new ConexionBD();
+        Assert.assertTrue(con.isConectado(), "No hay conexión creada. ");
+        con.desconectar();
+        Assert.assertFalse(con.isConectado(), "No se cerró conexión. ");
+    }
+    
+    @Ignore
+    public void verificarCargaProductos() {
+        ProductoDAO dao = new ProductoDAO();
+        Assert.assertTrue(dao.consultarProductos().size() > 0, "No se cargaron datos de productos. ");
+    }
+    
+    @Ignore
+    public void verificarCargaTipoReferencia() {
+        ProductoDAO dao = new ProductoDAO();
+        Assert.assertTrue(dao.cargarTipoReferencia().size() > 0, "No se cargaron datos de los tipos de referencia. ");
+    }
+    
+    @Ignore
+    public void verificarCargaEspecificaciones() {
+        ProductoDAO dao = new ProductoDAO();
+        Assert.assertTrue(dao.cargarEspecificaciones().size() > 0, "No se cargaron datos de las especificaciones. ");
+    }
+    @Ignore
+    public void verificarInsercionProducto() {
+        ProductoDAO dao = new ProductoDAO();
+        Producto p = new Producto("Cono de prueba", 1, 1);
+        int id = dao.guardarNuevoProducto(p);
+        Assert.assertTrue(id > 0, "No se guardó dato de un producto. ");
+        String sql = "DELETE FROM productos WHERE idProducto = " + id;
+        ConexionBD con = new ConexionBD();
+        int cant = con.ejecutarUpdate(sql);
+        Assert.assertTrue(cant == 1, "No se logró hacer limpieza del dato de prueba de un producto ingresado. ");
+        con.desconectar();
+    }   
+}
